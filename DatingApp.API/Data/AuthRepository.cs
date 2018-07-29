565c0ddbd2c00cbc5995d2419eefc25496183856
Antonio F Urbano <afurbano@gmail.com>
2018-07-29 17:01:01 +0200

    End of section 3

---
+++ DatingApp.API/Data/AuthRepository.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Threading.Tasks;
+using DatingApp.API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        public DataContext _context { get; }
+        public AuthRepository(DataContext context)
+        {
+            this._context = context;
+
+        }
+        public async Task<User> Login(string userName, string password)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == userName);
+            
+            if(user == null)
+                return null;
+
+            if(!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+                return null;
+
+            return user;
+        }
+
+        public async Task<User> Register(User user, string password)
+        {
+            byte [] passwordHash, passwordSalt;
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }        
+
+        public async Task<bool> UserExists(string userName)
+        {
+            if(await _context.Users.AnyAsync(x=>x.UserName == userName))
+                return true;
+
+            return false;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for(int i = 0; i < computedHash.Length; i++)
+                {
+                    if(computedHash[i] != passwordHash[i]) return false;
+                }
+            }
+            return true;
+        }
+    }
+}
