2b1f99efb199f72ec28ef715964660206ac9ea3b
Antonio F Urbano <afurbano@gmail.com>
2018-07-29 20:52:30 +0200

    End of section 5

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -72,7 +72,7 @@ namespace DatingApp.API.Controllers
 
             return Ok(new {
                 token = tokenHandler.WriteToken(token)
-            });            
+            });
         }
     }
 }
