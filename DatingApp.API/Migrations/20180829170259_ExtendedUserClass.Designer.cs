50b1613c4bff04c8ef9c35df28f7dd4debfa902a
Antonio F Urbano <afurbano@gmail.com>
2018-08-30 18:21:43 +0200

    End of section 8

--- DatingApp.API/Migrations/20180829170259_ExtendedUserClass.Designer.cs
+++ DatingApp.API/Migrations/20180829170259_ExtendedUserClass.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using DatingApp.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DatingApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20180829170259_ExtendedUserClass")]
+    partial class ExtendedUserClass
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -54,8 +56,6 @@ namespace DatingApp.API.Migrations
                     b.Property<string>("Gender");
 
                     b.Property<string>("Interests");
-
-                    b.Property<string>("Introduction");
 
                     b.Property<string>("KnownAs");
 
